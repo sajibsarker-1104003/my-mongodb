b6eefd028f3fd99d6e0bab519273a7e9432e9a92
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-06-09 19:30:52 +0600

    Mongoose Schema

--- index.js
+++ index.js
@@ -6,4 +6,26 @@ mongoose.connect('mongodb://localhost:27017/my-students',)
 })
 .catch(err=>{
   console.error("Connection Failed!!!");
+});
+
+// Schema=> Define the shape of the document
+
+const studentSchema=new mongoose.Schema({
+  firstName:String,
+  lastName:String,
+  dob:Date,
+  entryDate:{type:Date,default:Date.now},
+  passed:Boolean,
+  hobbies:[String],
+  parents:{
+    father:String,
+    mother:String,
+
+  },
+  subjects:[{
+    name:String,
+    subjectCode:Number,
+    marks:{type:Number,min:0,max:100}
+  }]
+
 })
