73f4a07524dd37ecc3bc82c5bd6d227b00b650cb
sajibsarker-1104003 <sajibpranto600@gmail.com>
2022-06-09 20:15:05 +0600

    Creating Model and Saving Document

--- index.js
+++ index.js
@@ -26,6 +26,44 @@ const studentSchema=new mongoose.Schema({
     name:String,
     subjectCode:Number,
     marks:{type:Number,min:0,max:100}
-  }]
-
+  }],
+ 
 })
+
+// Mongoose Model
+
+const Student=mongoose.model('Student',studentSchema);//class
+const student=new Student({
+  firstName:"Sajib",
+  lastName:"Sarker",
+  dob:new Date("04 January 1997"),
+  passed:true,
+  hobbies:["Travelling","Coding","Drawing"],
+  parents:{
+    father:"Sankar Sarker",
+    mother:"Uma Sarker"
+  },
+  subjects:[{
+    name:"Data Structure",
+    subjectCode:301,
+    marks:85
+  },
+  {
+    name:"Algorithm",
+    subjectCode:302,
+    marks:90
+  },
+  {
+    name:"AI",
+    subjectCode:303,
+    marks:76
+  },
+  {
+    name:"Machine Learning",
+    subjectCode:304,
+    marks:83
+  }]
+});//Object create
+
+student.save().then(data=>console.log(data)).catch(err=>console.log(err._message));
+
